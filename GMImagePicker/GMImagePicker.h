0e1615272c18d6cd936d5fda861494cdfb11ae07
Ngoc Luu <ngocluu.net@gmail.com>
2017-03-16 08:31:01 +0700

    Add Carthage support.

---
+++ GMImagePicker/GMImagePicker.h
@@ -0,0 +1,19 @@
+//
+//  GMImagePicker.h
+//  GMImagePicker
+//
+//  Created by Ngoc Luu on 3/16/17.
+//  Copyright © 2017 Ngoc Luu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GMImagePicker.
+FOUNDATION_EXPORT double GMImagePickerVersionNumber;
+
+//! Project version string for GMImagePicker.
+FOUNDATION_EXPORT const unsigned char GMImagePickerVersionString[];
+
+#import <GMImagePicker/GMImagePickerController.h>
+
+
